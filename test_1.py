b5c12d0b0405ebd8ba7b66ba864801a2b72fcd44
Jacob Puett <jake330@mail.fresnostate.edu>
2020-11-12 17:36:35 -0800

    finished exam

--- test_1.py
+++ test_1.py
@@ -38,3 +38,34 @@ return 100 * 10
 
 main
 """
+
+
+def option1():
+    return 100 * 10
+
+
+def option2():
+    amount = 1
+    list1 = []
+    for i in range(0, 10):
+        list1.append(amount)
+        amount *= 2
+    total = sum(list1)
+    return total
+
+
+def main():
+    answer = ""
+    var1 = option1()
+    var2 = option2()
+    print("from main: var1", var1, "var2", var2)
+    if var1 == var2:
+        answer = "Option 1 and option 2 pay the same"
+    elif var1 < var2:
+        answer = "Option 2 is better"
+    else:
+        answer = "Option 1 is better"
+    print(answer)
+
+
+main()
